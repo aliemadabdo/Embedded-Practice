d7dfc3c9ca1f05f84a383c1dff585768c89b13c2
aliemadabdo <a.e.a3426@gmail.com>
2023-07-07 02:23:52 +0300

    burst commit

---
+++ C Basics/neested_loop_pattern/src/U2Lec3Labs.c
@@ -0,0 +1,27 @@
+/*
+ ============================================================================
+ Name        : U2Lec3Labs.c
+ Author      : ali emad
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello from main function\n\n");
+
+	/* LAB 5
+	 * A program to print numbers from 1 to 9 as triangle with nested for loop */
+	for(int i = 10; i>0; i--){
+		for(int j = 10-i; j<10; j++){
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+	return 0;
+
+
+}
