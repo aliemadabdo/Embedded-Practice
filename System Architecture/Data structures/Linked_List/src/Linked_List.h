7a3d58dd7571257c9e62eccb2845a9f90dba2ea0
aliemadabdo <a.e.a3426@gmail.com>
2023-08-01 06:04:43 +0300

    ADD Linked_List.h and Linked_List.c
    set it as usable driver

--- System Architecture/Data structures/Linked_List/src/Linked_List.h
+++ System Architecture/Data structures/Linked_List/src/Linked_List.h
@@ -8,8 +8,32 @@
 #ifndef LINKED_LIST_H_
 #define LINKED_LIST_H_
 
+#define DEBUG 1
 
+struct Sdata{
+	int id;
+	char name[50];
+	float height;
+};
 
+struct Sstudent{
+	struct Sdata data;
+	struct Sstudent* next;
+};
 
+enum{
+	EXIT,
+	ADD_STUDENT,
+	DELET_STUDENT,
+	VIEW_STUDENTS,
+	DELET_ALL
+};
+
+void add(struct Sstudent* ptr_student);
+void add_student(void);
+void delete_student(void);
+void print_list();
+void delete(int ID);
+void delete_all(void);
 
 #endif /* LINKED_LIST_H_ */
